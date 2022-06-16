8df9d306ccf7a0fdf68c6f258f89c4d7df126e8d
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-16 14:52:15 +0530

    added test case to find max number at 3rd position

--- src/test/java/TestMaximumTest.java
+++ src/test/java/TestMaximumTest.java
@@ -10,8 +10,14 @@ public class TestMaximumTest {
     }
     @Test
     public void givenMaxNum_AtSecondPosition_ReturnSameNUmber() {
-        FindMaxNum num = new FindMaxNum(2, 10, 1);
-        int maxNumAtSecond = num.maxNumber();
+        FindMaxNum number = new FindMaxNum(2, 10, 1);
+        int maxNumAtSecond = number.maxNumber();
         Assert.assertEquals(10, maxNumAtSecond);
+    }
+    @Test
+    public void givenMaxNum_AtThirdPosition_ReturnSameNUmber() {
+        FindMaxNum number = new FindMaxNum(2, 2, 10);
+        int maxNumAtThird = (int) number.maxNumber();
+        Assert.assertEquals(10, maxNumAtThird);
     }
 }
