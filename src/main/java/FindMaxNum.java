4454215907bd327490ba02b7b03988cc2066a259
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-16 20:59:25 +0530

    added code to take more than 3 parameters for maxNumber method

--- src/main/java/FindMaxNum.java
+++ src/main/java/FindMaxNum.java
@@ -1,15 +1,6 @@
 import java.util.Arrays;
 
 public class FindMaxNum<T extends Comparable<T>>{
-    public T num1, num2, num3, num4, num5;
-
-    public FindMaxNum(T num1, T num2, T num3, T num4, T num5) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
-        this.num4 = num4;
-        this.num5 = num5;
-    }
 
     public FindMaxNum() {
     }
@@ -23,9 +14,7 @@ public class FindMaxNum<T extends Comparable<T>>{
         }
         return max;
     }
-    public <T extends Comparable <T>> T testMaximum() {
-       return (T) FindMaxNum.maxNumber(num1, num2, num3, num4, num5);
-    }
+
     public static <T extends Comparable<T>> void sort(T...num) {
         T temp;
         for(int i = 0; i < num.length; i++) {
--- src/test/java/TestMaximumTest.java
+++ src/test/java/TestMaximumTest.java
@@ -4,90 +4,67 @@ import org.junit.Test;
 public class TestMaximumTest {
     @Test
     public void givenIntegerNum_AtFirstPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum number = new FindMaxNum();
-        int maxNumAtFirst = (int) number.maxNumber(10, 1, 2);
+        FindMaxNum<Integer> number = new FindMaxNum();
+        int maxNumAtFirst = number.maxNumber(10, 1, 2);
         Assert.assertEquals(10, maxNumAtFirst);
         FindMaxNum.sort(10,1,2);
     }
     @Test
     public void givenIntegerNum_AtSecondPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum number = new FindMaxNum();
-        int maxNumAtSecond = (int) number.maxNumber(2, 10, 1);
+        FindMaxNum<Integer> number = new FindMaxNum();
+        int maxNumAtSecond = number.maxNumber(2, 10, 1);
         Assert.assertEquals(10, maxNumAtSecond);
         FindMaxNum.sort(2, 10, 1);
     }
     @Test
     public void givenIntegerNum_AtThirdPosition_WhenMax_ShouldReturnSameNUmber() {
         FindMaxNum<Integer> number = new FindMaxNum();
-        int maxNumAtThird = (int) number.maxNumber(2, 2, 10);
+        int maxNumAtThird = number.maxNumber(2, 2, 10);
         Assert.assertEquals(10, maxNumAtThird);
         FindMaxNum.sort(2, 2, 10);
     }
     @Test
     public void givenFloatNum_AtFirstPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum number = new FindMaxNum();
-        double maxNumAtFirst = (double) number.maxNumber(10.8, 2.6, 1.4);
+        FindMaxNum<Double> number = new FindMaxNum();
+        double maxNumAtFirst = number.maxNumber(10.8, 2.6, 1.4);
         Assert.assertEquals(10.8, maxNumAtFirst, 8);
         FindMaxNum.sort(10.8, 2.6, 1.4);
     }
 
     @Test
     public void givenFloatNum_AtSecondPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum number = new FindMaxNum();
-        double maxNumAtSecond = (double) number.maxNumber(2.6, 10.8, 1.4);
+        FindMaxNum<Double> number = new FindMaxNum();
+        double maxNumAtSecond = number.maxNumber(2.6, 10.8, 1.4);
         Assert.assertEquals(10.8, maxNumAtSecond, 8);
         FindMaxNum.sort(2.6, 10.8, 1.4);
     }
     @Test
     public void givenFloatNum_AtThirdPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum number = new FindMaxNum();
-        double maxNumAtThird = (double) number.maxNumber(2.6, 1.4, 10.8);
+        FindMaxNum<Double> number = new FindMaxNum();
+        double maxNumAtThird = number.maxNumber(2.6, 1.4, 10.8);
         Assert.assertEquals(10.8, maxNumAtThird, 8);
         FindMaxNum.sort(2.6, 1.4, 10.8);
     }
     @Test
     public void givenString_AtFirstPosition_WhenMax_ShouldReturnSameString() {
-        FindMaxNum string = new FindMaxNum();
-        String stringAtFirstPos = (String) string.maxNumber("peach", "apple", "banana");
+        FindMaxNum<String> string = new FindMaxNum();
+        String stringAtFirstPos = string.maxNumber("peach", "apple", "banana");
         Assert.assertEquals("peach", stringAtFirstPos);
         FindMaxNum.sort("peach", "apple", "banana");
     }
 
     @Test
     public void givenString_AtSecondPosition_WhenMax_ShouldReturnSameString() {
-        FindMaxNum string = new FindMaxNum();
-        String stringAtSecondPos = (String) string.maxNumber("apple", "peach", "banana");
+        FindMaxNum<String> string = new FindMaxNum();
+        String stringAtSecondPos = string.maxNumber("apple", "peach", "banana");
         Assert.assertEquals("peach", stringAtSecondPos);
         FindMaxNum.sort("peach", "apple", "banana");
     }
     @Test
     public void givenString_AtThirdPosition_WhenMax_ShouldReturnSameString() {
-        FindMaxNum string = new FindMaxNum();
-        String stringAtThirdPos = (String) string.maxNumber("apple", "banana", "peach");
+        FindMaxNum<String> string = new FindMaxNum();
+        String stringAtThirdPos = string.maxNumber("apple", "banana", "peach");
         Assert.assertEquals("peach", stringAtThirdPos);
         FindMaxNum.sort("apple", "banana", "peach");
-
-    }
-
-    @Test
-    public void givenValue_AtFirstPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum<Integer> number = new FindMaxNum(1, 1, 2, 10, 20);
-        int maxNumAtFirst = number.testMaximum();
-        Assert.assertEquals(20, maxNumAtFirst);
-        FindMaxNum.sort(1, 1, 2, 10, 20);
-    }
-    @Test
-    public void givenValue_AtSecondPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum<Double> number = new FindMaxNum(2.9, 10.8, 1.8, 2.6, 4.9);
-        Double maxNumAtSecond = number.testMaximum();
-        Assert.assertEquals(10.8, maxNumAtSecond, 8);
-        FindMaxNum.sort(2.9, 10.8, 1.8, 2.6, 4.9);
-    }
-    @Test
-    public void givenValue_AtThirdPosition_WhenMax_ShouldReturnSameNUmber() {
-        FindMaxNum<String> number = new FindMaxNum("apple", "banana", "peach", "ball", "goal");
-        String stringAtThirdPos =  number.testMaximum();
-        Assert.assertEquals("peach", stringAtThirdPos);
-        FindMaxNum.sort("apple", "banana", "peach", "ball", "goal");
     }
 }
