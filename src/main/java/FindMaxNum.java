0155a3e8a400292bef0c07cb42172d4e36710dba
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-16 19:51:22 +0530

    added code to take more than 3 parameters for maxNumber method

--- src/main/java/FindMaxNum.java
+++ src/main/java/FindMaxNum.java
@@ -1,24 +1,45 @@
-public class FindMaxNum<T extends Comparable<T>>{
-    public T num1, num2, num3;
+import java.util.Arrays;
 
-    public FindMaxNum(T num1, T num2, T num3) {
+public class FindMaxNum<T extends Comparable<T>>{
+    public T num1, num2, num3, num4, num5;
+
+    public FindMaxNum(T num1, T num2, T num3, T num4, T num5) {
         this.num1 = num1;
         this.num2 = num2;
         this.num3 = num3;
+        this.num4 = num4;
+        this.num5 = num5;
     }
 
     public FindMaxNum() {
     }
 
-    public static <T extends Comparable<T>> T maxNumber(T num1, T num2, T num3) {
-        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
-            return num1;
-        } else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
-            return num2;
-        } else
-            return num3;
+    public static <T extends Comparable<T>> T maxNumber(T...num) {
+        T max = num[0];
+        for(int i = 0; i < num.length; i++) {
+            if (num[i].compareTo(max) > 0) {
+                max = num[i];
+            }
+        }
+        return max;
     }
     public <T extends Comparable <T>> T testMaximum() {
-       return (T) FindMaxNum.maxNumber(num1, num2, num3);
+       return (T) FindMaxNum.maxNumber(num1, num2, num3, num4, num5);
     }
+    public static <T extends Comparable<T>> void sort(T...num) {
+        T temp;
+        for(int i = 0; i < num.length; i++) {
+            for(int j = i + 1; j < num.length; j++) {
+                if (num[i].compareTo(num[j]) < 0) {
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i] + " ");
+        }
+    }
+
 }
