08c6d03264bfb1d03d5e3c0da8c482892db563f6
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-16 15:46:09 +0530

    Refactored code to use generic method

--- src/main/java/FindMaxNum.java
+++ src/main/java/FindMaxNum.java
@@ -1,36 +1,11 @@
-public class FindMaxNum {
-    public Integer num1, num2, num3;
-    public Double num4, num5, num6;
+public class FindMaxNum<T extends Comparable>{
 
-    public FindMaxNum() {
-
-    }
-
-    public int maxNumber(Integer num1, Integer num2, Integer num3) {
+    public <T extends Comparable> T maxNumber(T num1, T num2, T num3) {
         if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0) {
             return num1;
         } else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0) {
             return num2;
         } else
             return num3;
-    }
-
-    public double maxNumber(Double num4, Double num5, Double num6) {
-        if ((num4.compareTo(num5)) > 0 && (num4.compareTo(num6)) > 0) {
-            return num4;
-        } else if (num5.compareTo(num6) > 0 && num5.compareTo(num4) > 0) {
-            return num5;
-        }
-        return num6;
-    }
-
-    public String maxNumber(String string1, String string2, String string3 ) {
-        if((string1.compareTo(string2)) >0 && (string1.compareTo(string3)) > 0) {
-            return string1;
-        }
-        else if(string2.compareTo(string3) > 0 && string2.compareTo(string1) > 0) {
-            return string2;
-        }
-        return string3;
     }
 }
--- src/test/java/TestMaximumTest.java
+++ src/test/java/TestMaximumTest.java
@@ -5,57 +5,57 @@ public class TestMaximumTest {
     @Test
     public void givenIntegerNum_AtFirstPosition_WhenMax_ShouldReturnSameNUmber() {
         FindMaxNum number = new FindMaxNum();
-        int maxNumAtFirst = number.maxNumber(10, 1, 2);
+        int maxNumAtFirst = (int) number.maxNumber(10, 1, 2);
         Assert.assertEquals(10, maxNumAtFirst);
     }
     @Test
     public void givenIntegerNum_AtSecondPosition_WhenMax_ShouldReturnSameNUmber() {
         FindMaxNum number = new FindMaxNum();
-        int maxNumAtSecond = number.maxNumber(2, 10, 1);
+        int maxNumAtSecond = (int) number.maxNumber(2, 10, 1);
         Assert.assertEquals(10, maxNumAtSecond);
     }
     @Test
     public void givenIntegerNum_AtThirdPosition_WhenMax_ShouldReturnSameNUmberr() {
         FindMaxNum number = new FindMaxNum();
-        int maxNumAtThird = number.maxNumber(2, 2, 10);
+        int maxNumAtThird = (int) number.maxNumber(2, 2, 10);
         Assert.assertEquals(10, maxNumAtThird);
     }
     @Test
     public void givenFloatNum_AtFirstPosition_WhenMax_ShouldReturnSameNUmber() {
         FindMaxNum number = new FindMaxNum();
-        double maxNumAtFirst = number.maxNumber(10.8, 2.6, 1.4);
+        double maxNumAtFirst = (double) number.maxNumber(10.8, 2.6, 1.4);
         Assert.assertEquals(10.8, maxNumAtFirst, 8);
     }
 
     @Test
     public void givenFloatNum_AtSecondPosition_WhenMax_ShouldReturnSameNUmber() {
         FindMaxNum number = new FindMaxNum();
-        double maxNumAtSecond = number.maxNumber(2.6, 10.8, 1.4);
+        double maxNumAtSecond = (double) number.maxNumber(2.6, 10.8, 1.4);
         Assert.assertEquals(10.8, maxNumAtSecond, 8);
     }
     @Test
     public void givenFloatNum_AtThirdPosition_WhenMax_ShouldReturnSameNUmber() {
         FindMaxNum number = new FindMaxNum();
-        double maxNumAtThird = number.maxNumber(2.6, 1.4, 10.8);
+        double maxNumAtThird = (double) number.maxNumber(2.6, 1.4, 10.8);
         Assert.assertEquals(10.8, maxNumAtThird, 8);
     }
     @Test
     public void givenString_AtFirstPosition_WhenMax_ShouldReturnSameString() {
         FindMaxNum string = new FindMaxNum();
-        String stringAtFirstPos = string.maxNumber("peach", "apple", "banana");
+        String stringAtFirstPos = (String) string.maxNumber("peach", "apple", "banana");
         Assert.assertEquals("peach", stringAtFirstPos);
     }
 
     @Test
     public void givenString_AtSecondPosition_WhenMax_ShouldReturnSameString() {
         FindMaxNum string = new FindMaxNum();
-        String stringAtSecondPos = string.maxNumber("apple", "peach", "banana");
+        String stringAtSecondPos = (String) string.maxNumber("apple", "peach", "banana");
         Assert.assertEquals("peach", stringAtSecondPos);
     }
     @Test
     public void givenString_AtThirdPosition_WhenMax_ShouldReturnSameString() {
         FindMaxNum string = new FindMaxNum();
-        String stringAtThirdPos = string.maxNumber("apple", "banana", "peach");
+        String stringAtThirdPos = (String) string.maxNumber("apple", "banana", "peach");
         Assert.assertEquals("peach", stringAtThirdPos);
     }
 }
