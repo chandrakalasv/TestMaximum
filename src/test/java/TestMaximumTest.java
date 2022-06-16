f00de917405cad5a90ffb543f4dcc08f0bd9cd23
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-16 14:48:28 +0530

    added test case to find max number given at second position

--- src/test/java/TestMaximumTest.java
+++ src/test/java/TestMaximumTest.java
@@ -5,7 +5,13 @@ public class TestMaximumTest {
     @Test
     public void givenMaxNum_AtFirstPosition_ReturnSameNUmber() {
         FindMaxNum number = new FindMaxNum(10, 1, 1);
-        int MaxNumAtFirst = (int) number.maxNumber();
-        Assert.assertEquals(10, MaxNumAtFirst);
+        int maxNumAtFirst =  number.maxNumber();
+        Assert.assertEquals(10, maxNumAtFirst);
+    }
+    @Test
+    public void givenMaxNum_AtSecondPosition_ReturnSameNUmber() {
+        FindMaxNum num = new FindMaxNum(2, 10, 1);
+        int maxNumAtSecond = num.maxNumber();
+        Assert.assertEquals(10, maxNumAtSecond);
     }
 }
